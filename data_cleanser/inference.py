4901695db253bf856076ac41d7026cb2a6aec121
Navdeep Beniwal <navdeep.beniwal1997@gmail.com>
2024-03-31 23:35:42 +1100

    done: numeric values inference

--- data_cleanser/inference.py
+++ data_cleanser/inference.py
@@ -1,63 +1,113 @@
 import pandas as pd
 import numpy as np
 
+MAX_INTEGER_CHECKABLE_FLOAT = 2.0 ** 53 # Max integer value that can be checked appropriately via is_integer()
+INFERENCE_THRESHOLD_PERCENTAGE = 0.5 # Threshold percentage of valid values in a data column to accurately infer the type
+
 """
 Method to return the data-type of the dataframe column passed, returns Object if not able to determine
 """
+class InferedDataType:
+    OBJECT = 'object'
+    INTEGER = 'int64'
+    INT64 = 'int64'
+    INT32 = 'int32'
+    INT16 = 'int16'
+    INT8 = 'int8'
+    FLOAT = 'float64'
+    FLOAT64 = 'float64'
+    FLOAT32 = 'float32'
 
-def infer_data_type(dataframe, col):
-    default_type = 'object'
+def infer_formatted_numeric_type(data_column):
+    # Matching for comma ',' or currency symbol '$' separated integers or floats values
+    comma_and_decimal_pattern = r'^(\$?\d{1,3}(,\d{3})*(\.\d+)?$)|(\d{1,3}(,\d{3})*(\.\d+)?$)'
+    matches = data_column.astype(str).str.match(comma_and_decimal_pattern, na=False).sum()
     
-    """
-    infering numeric types i.e (int64, int32,int16, int8)
-    """ 
-    print("Received data type:", dataframe[col].dtype)
-    df_converted = pd.to_numeric(dataframe[col], errors='coerce')
-    print("Converted data type:", df_converted.dtype)
+    # If more than the threshold number of the column data is inferred as numeric, return a numeric type i.e. integer or float
+    if matches / len(data_column) > INFERENCE_THRESHOLD_PERCENTAGE:
+        contains_float = any('.' in str(x) for x in data_column)
+        return InferedDataType.FLOAT64 if contains_float else InferedDataType.INT64
+    
+    # Return object as default if no numeric type could be inferred
+    return InferedDataType.OBJECT
 
-    total_values_count = len(df_converted)
-    numeric_values_count = total_values_count - df_converted.isna().sum()
+"""
+Function to infer numeric types i.e (int64, int32,int16, int8, float64, float32)
+"""
+def infer_numeric_type(data_column):
+    infered_data_type = InferedDataType.OBJECT # Default value if numeric data not inferred
+    dc_converted = pd.to_numeric(data_column, errors='coerce') # Data column converted to numeric type using pandas
     
-    print("Numeric values count:", numeric_values_count)
-    
-    if pd.api.types.is_integer_dtype(df_converted):
-        if numeric_values_count > total_values_count / 2:  # Major proportion of the values are numeric
-            try:
-                col_min = dataframe[col].min()
-                col_max = dataframe[col].max()
+    total_values_count = len(dc_converted)
+    non_na_values_count = total_values_count - dc_converted.isna().sum() # Non-formatted numeric values
 
-                if col_min >= np.iinfo(np.int8).min and col_max <= np.iinfo(np.int8).max:
-                    return 'int8'
-                elif col_min >= np.iinfo(np.int16).min and col_max <= np.iinfo(np.int16).max:
-                    return 'int16'
-                elif col_min >= np.iinfo(np.int32).min and col_max <= np.iinfo(np.int32).max:
-                    return 'int32'
-                else:
-                    return 'int64'
-            except (TypeError):
-                # handling when the min, max are not numeric values
-                return 'int64'
+    if pd.api.types.is_integer_dtype(dc_converted):
+        infered_data_type = InferedDataType.INTEGER
+    elif pd.api.types.is_float_dtype(dc_converted):
+        infered_data_type = InferedDataType.INTEGER # Setting to INTEGER unless a float value is found
+
+        for value in dc_converted:
             
-    elif pd.api.types.is_float_dtype(df_converted):
-        if numeric_values_count > total_values_count / 2:  # Major proportion of the values are numeric
-            try:
-                col_min = dataframe[col].min()
-                col_max = dataframe[col].max()
-
-                if col_max <= np.finfo(np.float32).max:
-                    return 'float32'
-                else:
-                    return 'float64'
-            
-            except (TypeError):
-                # handling when the min, max are not numeric values
-                return 'float64'
-    
-    else: # Major proportion are not numeric
+            if not np.isnan(value):
+                if value > MAX_INTEGER_CHECKABLE_FLOAT or not float(value).is_integer():
+                    infered_data_type = InferedDataType.FLOAT
+                    break
+    else:
         pass
 
+    if non_na_values_count / total_values_count > INFERENCE_THRESHOLD_PERCENTAGE: # More than threshold percentage of the values are numeric
+        if infered_data_type == InferedDataType.INTEGER: # Check if converted dataframe is of integer type
+            try:
+                col_min = dc_converted.min()
+                col_max = dc_converted.max()
+
+                if col_min >= np.iinfo(np.int8).min and col_max <= np.iinfo(np.int8).max:
+                    return InferedDataType.INT8
+                elif col_min >= np.iinfo(np.int16).min and col_max <= np.iinfo(np.int16).max:
+                    return InferedDataType.INT16
+                elif col_min >= np.iinfo(np.int32).min and col_max <= np.iinfo(np.int32).max:
+                    return InferedDataType.INT32
+                else:
+                    return InferedDataType.INT64
+            except (TypeError):
+                    # Returning default 'int64' when the min, max are not numeric values
+                    return InferedDataType.INT64
+
+        else: # Check if converted dataframe is of floating type
+            try:
+                col_min = dc_converted.min()
+                col_max = dc_converted.max()
+
+                if col_max <= np.finfo(np.float32).max:
+                    return InferedDataType.FLOAT32
+                else:
+                    return InferedDataType.FLOAT64
+
+            except (TypeError):
+                # Returning default 'float64' when the min, max are not numeric values
+                return InferedDataType.FLOAT64
+            
+    else:
+        # Checking for formatted numeric strings
+        inferred_formatted_numeric_type = infer_formatted_numeric_type(data_column)
+
+        if inferred_formatted_numeric_type == InferedDataType.FLOAT64 or inferred_formatted_numeric_type == InferedDataType.INT64:
+            return inferred_formatted_numeric_type
+        
+        return InferedDataType.OBJECT
+
+def infer_data_type(dataframe, col):
+    data_column = dataframe[col]
+    infered_data_type = data_column.dtype
+    print("Received data type:", infered_data_type) # TODO: TBR
+
+    # Inferring numeric data type
+    if infered_data_type == InferedDataType.OBJECT or infered_data_type == InferedDataType.FLOAT64 or infered_data_type == InferedDataType.INT64:
+        infered_data_type = infer_numeric_type(data_column)
     
-    return default_type
+
+    return infered_data_type
+
 
 """
 Method to return the data-types of all of the columns in the dataframe passed
--- data_cleanser/tests/inference.py
+++ data_cleanser/tests/inference.py
@@ -25,10 +25,11 @@ class TestInference(unittest.TestCase):
         df = pd.DataFrame({'col': [2**7 - 1, -2**7]})
         self.assertEqual(infer_data_type(df, 'col'), 'int8')
 
+    # TODO: the method call needs to return an 'int64' given all of the values except NaN are integers - DD
     def test_int64_inference_with_nan(self):
         # Integers that fit within int64 but not int32, with NaN values included
-        df = pd.DataFrame({'col': [2**31, -2**31 - 1, 3, None, 'not a number']})
-        self.assertEqual(infer_data_type(df, 'col'), 'float64')
+        df = pd.DataFrame({'col': [2**31, -2**31 - 1, 3, None]})
+        self.assertEqual(infer_data_type(df, 'col'), 'int64')
 
     def test_float64_inference(self):
         # Float values that require float64 precision
@@ -39,6 +40,48 @@ class TestInference(unittest.TestCase):
         # Float values that fit within float32
         df = pd.DataFrame({'col': [3.402823e+30, -3.402823e+30]})
         self.assertEqual(infer_data_type(df, 'col'), 'float32')
+
+    def test_float64_with_nan(self):
+        # Float values that require float64 precision
+        df = pd.DataFrame({'col': [1.7976931348623157e+308, -1.7976931348623157e+308, 12.5, None, 'not a number']})
+        self.assertEqual(infer_data_type(df, 'col'), 'float64')
+
+    def test_mixed_numeric_inference(self):
+        # Integers, Floats and NaN all mixed in the data
+        mixed_data = [100, 200.5, np.nan, -500, 350.0, 400, 1.5e2, -2.5e2, 0, 'not a number', None, 230]
+        df = pd.DataFrame({'col': mixed_data})
+        self.assertEqual(infer_data_type(df, 'col'), 'float32')
+
+    def test_numeric_string_inference(self):
+        # Numeric values stored as strings
+        numbers_as_string = ['1.7976931348623157e+308', '-1.7976931348623157e+308']
+        df = pd.DataFrame({'col': numbers_as_string})
+        self.assertEqual(infer_data_type(df, 'col'), 'float64')
+
+    def test_numeric_commas_inference(self):
+        # Numeric values with commas as thousands separators
+        mixed_data = ['1,000', '2,500', '10,000', '100,000', '1,000,000']
+        df = pd.DataFrame({'col': mixed_data})
+        self.assertEqual(infer_data_type(df, 'col'), 'int64')
+
+    def test_numeric_commas_mixed_with_floats_inference(self):
+        # Mixed data with numbers containing commas and floats
+        mixed_data = ['1,000', '2,500', '10,000', '100,000', '1,000,000', '3.14', '6,543.21']
+        df = pd.DataFrame({'col': mixed_data})
+        self.assertEqual(infer_data_type(df, 'col'), 'float64')
+
+    def test_numeric_commas_mixed_with_non_numeric_inference(self):
+        # Mixed data with numbers containing commas, floats, and non-numeric values
+        mixed_data = ['1,000', '2,500', '10,000', '100,000', '1,000,000', '3.14', '6,543.21', 'not a number']
+        df = pd.DataFrame({'col': mixed_data})
+        self.assertEqual(infer_data_type(df, 'col'), 'float64')
+
+    def test_numeric_commas_with_decimal_places_inference(self):
+        # Numeric values with commas as thousands separators and decimal places
+        mixed_data = ['1,000.50', '2,500.75', '10,000.25', '100,000.60', '1,000,000.99']
+        df = pd.DataFrame({'col': mixed_data})
+        self.assertEqual(infer_data_type(df, 'col'), 'float64')
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
